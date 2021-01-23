cd0f50e804783ea6c965c4cb2aca2e2a2f243bd2
LalitDeol <lalitdeol95@gmail.com>
2021-01-23 22:37:14 +0530

    done

--- src/index.js
+++ src/index.js
@@ -2,30 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-let curdate = new Date( );
+let curdate = new Date();
 curdate = curdate.getHours();
 let Greeting = "";
-const cssStyle = {}; 
-  
-if( curdate>=1 && curdate <12){
+const cssStyle = {};
+
+if (curdate >= 1 && curdate < 12) {
   Greeting = 'Good Morning';
   cssStyle.color = 'green'
-}else if (curdate>=12 && curdate <19){
- Greeting = " Good Afternoon";
- cssStyle.color = 'orange'
-}else{
+} else if (curdate >= 12 && curdate < 19) {
+  Greeting = " Good Afternoon";
+  cssStyle.color = 'orange'
+} else {
   Greeting = " Goodnight";
   cssStyle.color = 'black';
 
 }
 
-
-
 ReactDOM.render(
   <>
-  <div>
-  <h1> Hello Lalit Deol , &nbsp;  < span style={cssStyle} >{Greeting} </span> </h1>
-  </div>
+    <div>
+      <h1> Hello Lalit Deol , &nbsp;  < span style={cssStyle} >{Greeting} </span> </h1>
+    </div>
   </>,
   document.getElementById('root')
 );
